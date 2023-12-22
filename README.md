58e0ba057b37fcad2ca28a6ca451f11ecc56d4cf
James Xu <xumingmingv@gmail.com>
2023-12-22 16:15:14 +0800

    Support placeholders

--- README.md
+++ README.md
@@ -53,3 +53,63 @@ Password: test_pass
 [Thread2-2] Status: OK, RT: 901ms
 [Thread3-2] Status: OK, RT: 850ms
 ```
+
+### Run sql with placeholders
+
+You can define placeholder in sql, e.g. we can define a placeholder named `id` like this:
+
+```sql
+select * from nation where n_nationkey = __id__
+```
+
+And when running, you can specify how to fill real value by:
+
+```bash
+dataops run test.sql 10 -p id:1,100 -v
+```
+
+`id:1,100` here means `__id__` in SQL will be replaced by a random value in range: `[1, 100)`, the effect is:
+
+```bash
+➜  poc dataops run test.sql 10 -p id:1,100 -v
+Host    : mysql.test.com
+Db      : test_db
+User    : test_user
+Password: test_pass
+
+Thread 0 will run SQL: select * from nation where n_nationkey = 82
+Thread 1 will run SQL: select * from nation where n_nationkey = 59
+Thread 2 will run SQL: select * from nation where n_nationkey = 68
+Thread 3 will run SQL: select * from nation where n_nationkey = 51
+Thread 4 will run SQL: select * from nation where n_nationkey = 64
+Thread 5 will run SQL: select * from nation where n_nationkey = 16
+Thread 6 will run SQL: select * from nation where n_nationkey = 96
+Thread 7 will run SQL: select * from nation where n_nationkey = 42
+Thread 8 will run SQL: select * from nation where n_nationkey = 23
+Thread 9 will run SQL: select * from nation where n_nationkey = 30
+[Thread6-0] Status: OK, RT: 28ms
+[Thread9-0] Status: OK, RT: 27ms
+[Thread3-0] Status: OK, RT: 27ms
+```
+
+You can define more than one placeholders:
+
+```sql
+select * from nation where n_nationkey = __id__ and name='hello__name__'
+```
+
+Then run with:
+
+```bash
+dataops run test.sql 10 -p id:1,100;name:1,100 -v
+```
+
+
+### Show verbose log
+
+```bash
+dataops run test.sql 10 -v
+```
+
+Enjoy!
+
--- src/main/java/io/github/xumingming/dataops/RunCommand.java
+++ src/main/java/io/github/xumingming/dataops/RunCommand.java
@@ -13,9 +13,12 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
+import static io.github.xumingming.beauty.Beauty.draw;
 import static io.github.xumingming.beauty.Beauty.drawError;
 import static io.github.xumingming.dataops.Utils.read;
 
@@ -29,6 +32,17 @@ public class RunCommand
     @CommandLine.Parameters(index = "1", defaultValue = "1", description = "parallelism")
     private int parallelism;
 
+    @CommandLine.Option(
+            names = {"-p", "--placeholders"},
+            description = "placeholders")
+    private String placeholders;
+
+    @CommandLine.Option(
+            names = {"-v", "--verbose"},
+            description = "whether show verbose(detailed) information",
+            defaultValue = "false")
+    private boolean verbose;
+
     @Override
     public Integer call()
             throws Exception
@@ -39,10 +53,21 @@ public class RunCommand
         String str = Beauty.detail(conf, Arrays.asList(Column.column("Host", (DataOpsConf theConf) -> conf.getHost()), Column.column("Db", (DataOpsConf theConf) -> conf.getDb()), Column.column("User", (DataOpsConf theConf) -> conf.getUser()), Column.column("Password", (DataOpsConf theConf) -> conf.getPassword())), Color.NONE);
         System.out.println(str);
 
+        List<PlaceholderInfo> placeholderInfos = new ArrayList<>();
+        if (placeholders != null && !placeholders.isEmpty()) {
+            placeholderInfos = parsePlaceholders(placeholders);
+        }
+
         List<RunSqlTask> tasks = new ArrayList<>(parallelism);
         ConnectionInfo conn = new ConnectionInfo(conf.getHost(), 3306, conf.getDb(), conf.getUser(), conf.getPassword());
         for (int i = 0; i < parallelism; i++) {
-            RunSqlTask task = new RunSqlTask("Thread" + i, conn, read(sqlPath));
+            String sql = read(sqlPath);
+            sql = rewrite(sql, placeholderInfos);
+            if (verbose) {
+                draw(String.format("Thread %s will run SQL: %s", i, sql));
+            }
+
+            RunSqlTask task = new RunSqlTask("Thread" + i, conn, sql);
             tasks.add(task);
             task.start();
         }
@@ -52,6 +77,116 @@ public class RunCommand
         }
 
         return 0;
+    }
+
+    public static class PlaceholderInfo
+    {
+        private String key;
+        private int start;
+        private int end;
+
+        public PlaceholderInfo(String key, int start, int end)
+        {
+            this.key = key;
+            this.start = start;
+            this.end = end;
+        }
+
+        public String getKey()
+        {
+            return key;
+        }
+
+        public void setKey(String key)
+        {
+            this.key = key;
+        }
+
+        public int getStart()
+        {
+            return start;
+        }
+
+        public void setStart(int start)
+        {
+            this.start = start;
+        }
+
+        public int getEnd()
+        {
+            return end;
+        }
+
+        public void setEnd(int end)
+        {
+            this.end = end;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            PlaceholderInfo that = (PlaceholderInfo) o;
+            return start == that.start && end == that.end && key.equals(that.key);
+        }
+
+        @Override
+        public int hashCode()
+        {
+            return Objects.hash(key, start, end);
+        }
+
+        @Override
+        public String toString()
+        {
+            return "PlaceholderInfo{" +
+                    "key='" + key + '\'' +
+                    ", start=" + start +
+                    ", end=" + end +
+                    '}';
+        }
+    }
+
+    public static List<PlaceholderInfo> parsePlaceholders(String placeholders)
+    {
+        String[] topParts = placeholders.split(";");
+        List<PlaceholderInfo> ret = new ArrayList<>();
+        for (String topPart : topParts) {
+            String[] secondParts = topPart.split(":");
+            if (secondParts.length != 2) {
+                throw new RuntimeException("Invalid placeholder: " + placeholders);
+            }
+
+            String key = secondParts[0].trim();
+            String value = secondParts[1].trim();
+            String[] valueParts = value.split(",");
+            if (valueParts.length != 2) {
+                throw new RuntimeException("Invalid placeholder: " + placeholders);
+            }
+
+            int start = Integer.parseInt(valueParts[0].trim());
+            int end = Integer.parseInt(valueParts[1].trim());
+
+            ret.add(new PlaceholderInfo(key, start, end));
+        }
+
+        return ret;
+    }
+
+    public static String rewrite(String sql, List<PlaceholderInfo> placeholderInfos)
+    {
+        String ret = sql;
+        for (PlaceholderInfo info : placeholderInfos) {
+            int value = info.start + new Random().nextInt(info.end - info.start);
+            ret = ret.replaceAll("__" + info.key + "__", String.valueOf(value));
+        }
+
+        return ret;
     }
 
     public static class RunSqlTask
